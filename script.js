a2220093d6edc2b06dead1afeea308476a8d9002
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-06-04 15:33:40 +0530

    animation added

--- script.js
+++ script.js
@@ -280,3 +280,39 @@ errorBtn.forEach(button => {
     });
 
 });
+
+/* =========================================
+   SCROLL SECTION ANIMATION
+========================================= */
+
+const observer = new IntersectionObserver(
+
+(entries)=>{
+
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("active");
+
+        }
+
+    });
+
+},
+
+{
+    threshold:0.2
+}
+
+);
+
+/* ALL ELEMENTS */
+
+const hiddenElements = document.querySelectorAll(
+
+".reveal, .reveal-left, .reveal-right, .zoom-in"
+
+);
+
+hiddenElements.forEach((el)=> observer.observe(el));
